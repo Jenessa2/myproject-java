77321478defcd85f822d7af57032331809c8e4c4
Jenessa <jenessa.chang@gmail.com>
2021-11-09 13:48:20 +0800

    initial commit

---
+++ src/main/java/com/c/Guess1To10.java
@@ -0,0 +1,12 @@
+package com.c;
+
+import java.util.Random;
+
+public class Guess1To10 {
+    public static void main(String[] args) {
+        //產生1-10亂數
+        System.out.println("Hello");
+//        Random random = new Random();
+//        int secret = random.nextInt(10)+1;
+    }
+}
